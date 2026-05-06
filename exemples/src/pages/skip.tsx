28ca772db640a24314d35d7758587022642c9f79
Justin Halle <shpaw415@gmail.com>
2026-05-06 18:22:47 -0400

    skip cache now implemented and tested

---
+++ exemples/src/pages/skip.tsx
@@ -0,0 +1,34 @@
+"use dynamic";
+import { useLoader } from "frame-master-plugin-cloudflare-pages-dynamic-ssr/client/hooks";
+import {
+	createLoader,
+	createPageConfig,
+} from "frame-master-plugin-cloudflare-pages-dynamic-ssr/server";
+
+export const ssr_configs = createPageConfig({
+	callback(ctx) {
+		return {
+			skipCache: true,
+		};
+	},
+});
+
+export const loader_test = createLoader({
+	name: "test",
+	async callback() {
+		return {
+			message: "This is a test loader",
+		};
+	},
+});
+
+export default function Invalid() {
+	const data = useLoader(loader_test);
+	return (
+		<div>
+			<h1>No cache Page</h1>
+			<p>This page is used to test the cache behavior.</p>
+			<p>Loader message: {data?.message}</p>
+		</div>
+	);
+}
